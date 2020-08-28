7136734eb0c7c9f9e41db8344527db5e2fe1ca52
redelization <ricardosmithco@gmail.com>
2020-08-27 22:43:29 -0500

    first commit: adding my spring project to git

---
+++ src/main/java/com/myeliteedge/myeliteedge/MyeliteedgeApplication.java
@@ -0,0 +1,13 @@
+package com.myeliteedge.myeliteedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeliteedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeliteedgeApplication.class, args);
+	}
+
+}
